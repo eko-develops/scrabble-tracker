f73265a81d90e42ba329b2c31b97c4237c2b3534
ekodevelops <ekodevelops@gmail.com>
2022-01-18 10:21:44 -0800

    Get form data

--- src/main.js
+++ src/main.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+const searchButton = document.querySelector(".form-button");
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+  const inputValue = document.querySelector(".form-container form input").value;
+
+  console.log(inputValue);
+};
+
+searchButton.addEventListener("click", (e) => handleSubmit(e));
