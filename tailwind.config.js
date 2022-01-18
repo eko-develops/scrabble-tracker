5ad0592ce1dbba88083dbbbe115a994f0ff77214
ekodevelops <ekodevelops@gmail.com>
2022-01-17 16:55:29 -0800

    Add project files

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      poppins: ["Poppins", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
